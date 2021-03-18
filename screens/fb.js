3303b24ed40e4ebc7513c7f005c9d8db22c26838
namko-star <70060024+namko-star@users.noreply.github.com>
2021-03-18 14:41:17 +0530

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Facebook extends React.Component {
+    render() {
+        <View>
+            <Text>placeholder text</Text>
+        </View>
+    }
+}
---
+++ screens/in.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Instagram extends React.Component {
+    render() {
+        <View>
+            <Text>placeholder text</Text>
+        </View>
+    }
+}
